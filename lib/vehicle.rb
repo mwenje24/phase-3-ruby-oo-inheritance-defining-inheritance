e56a9d25338b5dabec94936343fee59b0c6d9987
Mwenje <mwenje24@gmail.com>
2022-08-21 09:37:44 +0300

    completed assignment

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,15 @@
 class Vehicle
+    attr_accessor :wheel_size, :wheel_number
 
+    def initialize(wheel_size, wheel_numer)
+        @wheel_size=wheel_size
+        @wheel_number=wheel_numer
+    end
+
+    def go
+        "vrrrrrrrooom!"
+    end
+    def fill_up_tank
+        "filling up!"
+    end
 end
